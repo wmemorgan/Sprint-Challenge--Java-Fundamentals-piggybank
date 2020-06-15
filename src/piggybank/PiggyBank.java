a9cb378cd572ef3268e7fe1140ab03a1111ed2e8
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-15 23:27:28 +0800

    Add
    Refactor withdraw method
    
    Refactor getAllDeposits method
    
    Add data reset methods
    
    Refactor withdraw method

--- src/piggybank/PiggyBank.java
+++ src/piggybank/PiggyBank.java
@@ -8,8 +8,8 @@ import java.util.Map;
 
 public class PiggyBank {
 
-    String pattern = "$0";
-    DecimalFormat decimalFormat = new DecimalFormat(pattern);
+    protected double totalListValue = 0.0;
+    DecimalFormat decimalFormat = new DecimalFormat();
 
     public List<Money> piggyBank = new ArrayList<>();
     Map<Double, Integer> vault = new HashMap<>();
@@ -23,73 +23,92 @@ public class PiggyBank {
         piggyBank.add(new Dime(7));
         piggyBank.add(new Dollar());
         piggyBank.add(new Penny(10));
+    }
 
-        System.out.println("\npiggyBank: " + piggyBank.get(3).getFaceValue() + "\n");
+    public void removeEmptyDenom() {
+        // Remove empty denominations
+        piggyBank.removeIf(m -> m.getQuantity() == 0);
+    }
 
-        // Aggregate deposits
-        for (Money m : piggyBank) {
-            double denom = m.getFaceValue();
-
-            if (vault.containsKey(denom)) {
-                int updatedTotalQuantity = vault.get(denom) + m.getQuantity();
-                vault.put(denom, updatedTotalQuantity);
-            } else {
-                vault.put(denom, m.getQuantity());
-            }
-        }
+    public void resetListValue() {
+        this.totalListValue = 0.0;
     }
 
     public void getAllDeposits() {
-        // Print contents
-        System.out.println("Piggy Bank deposits:");
-        for (
+        // Clear empty denominations
+        removeEmptyDenom();
+        String pattern = "$0";
+        decimalFormat.applyPattern(pattern);
 
-        Money m : piggyBank) {
+        // Print contents
+        System.out.println("\nPiggy Bank contains:");
+        for (Money m : piggyBank) {
             if (m.getName().contains("Dollar")) {
+
                 System.out.println(decimalFormat.format(m.getTotalValue()));
             } else {
-                System.out.println(m.quantity + " " + m.getName());
+                System.out.println(m.getQuantity() + " " + m.getName());
             }
         }
+        
+        // Get cash on hand
+        calcTotalDeposits();
     }
     
-    public void getTotalDeposits() {
-    
-        double sum = 0.0;
-        for (HashMap.Entry mapElem : vault.entrySet()) {
-            double denomination = (double) mapElem.getKey();
-            double quantity = Double.valueOf((int) mapElem.getValue());
-            sum += denomination * quantity;
+    public void calcTotalDeposits(){
+        // Clear empty denominations
+        removeEmptyDenom();
+        // Reset list value
+        resetListValue();
+        // Calculate current total list value
+        for (Money m : piggyBank) {
+            this.totalListValue += m.getTotalValue();
         }
 
         decimalFormat.applyPattern("$0.00");
-        System.out.println("\nThe piggy bank holds " + decimalFormat.format(sum));
+        System.out.println("\nThe piggy bank holds " + decimalFormat.format(this.totalListValue));
     }
 
     public void withdraw(double amount) {
 
-        decimalFormat.applyPattern("$0.00");
-        System.out.println("\nWithdrawal amount: " + decimalFormat.format(amount));
-        double[] denom = { 0.01, 0.05, 0.1, 0.25, 1.0 };
-
-        for (int i = denom.length - 1; i > 0; i--) {
-            int denomQuantity = (int) (Math.floor(amount / denom[i]));
-            if (denomQuantity > 0 && vault.get(denom[i]) > 0) {
-                double totalValue = (double) vault.get(denom[i]) * denom[i];
-                double denomAmt = denomQuantity * denom[i];
-                double subtractAmt = Math.min(denomAmt, totalValue);
-
-                // Update denomination quantity
-                vault.put(denom[i], vault.get(denom[i]) - Math.min(denomQuantity, vault.get(denom[i])));
-                
-                // Decrement withdrawal amount
-                amount -= subtractAmt;
-                amount = Math.round(amount * 100.0) / 100.0;
-            }
+        if (amount <= 0.00) {
+            System.out.println("\nInvalid number. Please enter valid withdrawal");
         }
 
-        //System.out.println("Current vault contents: " + vault);
-        // Get cash on hand
-        getTotalDeposits();
+        else if (amount > this.totalListValue) {
+            System.out.println("\nInsufficient funds. Please enter a lower amount");
+        }
+
+        else {
+            decimalFormat.applyPattern("$0.00");
+            System.out.println("\nWithdraw " + decimalFormat.format(amount) + " from the piggy bank");
+
+            for (Money m : piggyBank) {
+                // Get available amount of specified denomination
+                double denomCashValue = amount / m.getFaceValue();
+                // Round to nearest hundredth
+                denomCashValue = Math.round(denomCashValue * 100.0) / 100.0;
+                int denomQuantity = (int) (Math.floor(denomCashValue));
+
+                // System.out.println("denomQty: " + denomQuantity);
+                if (denomQuantity > 0 && m.getQuantity() > 0) {
+                    //System.out.println(m.getQuantity() + " " + m.getName() + " available");
+                    double denonAmt = denomQuantity * m.getFaceValue();
+                    double subtractAmt = Math.min(denonAmt, m.getTotalValue());
+
+                    // Update denomination quantity
+                    int qtyToSubtract = Math.min(denomQuantity, m.getQuantity());
+                    // System.out.println("Qty to subtract: " + qtyToSubtract);
+                    m.subtract(qtyToSubtract);
+                    amount -= subtractAmt;
+                    // Round to the nearest hundredth
+                    amount = Math.round(amount * 100.0) / 100.0;
+                    // System.out.println("CURRENT AMT: " + amount);
+                }
+            }
+
+            // Get current piggy bank contents
+            getAllDeposits();
+        }
     }
 }
