fd75df0e855543d592496113ecb01f82d92d5322
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-15 13:56:33 +0800

    Create piggyBank collection

--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -1,32 +1,26 @@
 package piggybank;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("*** Cafe Morgan Piggy Bank & Trust ***");
-        Dollar dollar = new Dollar(5);
-        System.out.println(dollar);
-        Dollar dollar2 = new Dollar();
-        System.out.println(dollar2);
-        
-        Quarter quarter1 = new Quarter();
-        Quarter quarter2 = new Quarter(5);
-        System.out.println(quarter1);
-        System.out.println(quarter2);
-        
-        Dime dime1 = new Dime();
-        Dime dime2 = new Dime(7);
-        System.out.println(dime1);
-        System.out.println(dime2);
-        
-        Nickel nickel1 = new Nickel();
-        Nickel nickel2 = new Nickel(3);
-        System.out.println(nickel1);
-        System.out.println(nickel2);
-        
-        Penny penny1 = new Penny();
-        Penny pennies = new Penny(37);
-        System.out.println(penny1);
-        System.out.println(pennies);
+        String pattern = "$0";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+
+        List<Money> piggyBank = new ArrayList<>();
+
+        System.out.println("\n*** Welcome to Cafe Morgan Piggy Bank & Trust ***\n");
+
+        // Deposit money to bank
+        piggyBank.add(new Quarter());
+        piggyBank.add(new Dime());
+        piggyBank.add(new Dollar(5));
+        piggyBank.add(new Nickel(3));
+        piggyBank.add(new Dime(7));
+        piggyBank.add(new Dollar());
+        piggyBank.add(new Penny(10));
     }
 }
