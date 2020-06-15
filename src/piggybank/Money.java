716e66b92e8605f31cd3a68eaee3e664d25c7cb5
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-15 13:02:47 +0800

    Create abstract class 'Money'

---
+++ src/piggybank/Money.java
@@ -0,0 +1,61 @@
+package piggybank;
+
+import java.text.DecimalFormat;
+
+abstract class Money {
+
+    String pattern = "$0.00";
+    DecimalFormat decimalFormat = new DecimalFormat(pattern);
+
+    protected static int maxId = 0;
+
+    protected int id;
+    protected String name;
+    protected int quantity;
+
+    public Money(int quantity) {
+        maxId++;
+        id = maxId;
+        name = "placeholder";
+        this.quantity = quantity;
+    }
+
+    public Money() {
+        maxId++;
+        id = maxId;
+        name = "placeholder";
+        quantity = 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    };
+
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+    
+    abstract double getFaceValue();
+
+    @Override
+    public String toString() {
+        return name + " " + "quantity: " + quantity + " face value: " + decimalFormat.format(getFaceValue());
+    }
+
+}
